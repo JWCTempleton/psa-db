8fe5380c4e756f364bbd832635e86acf91aadb17
Jacob Templeton <jacob.templeton@gmail.com>
2023-01-10 17:37:54 -0800

    Add input options for card submission form

--- psa-db/src/App.tsx
+++ psa-db/src/App.tsx
@@ -1,15 +1,48 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import "./App.css";
 import Card from "./components/Card";
-import Form from "./components/Form";
+
+const styles = {
+  display: "flex",
+  flexDirection: "column",
+  width: "30rem",
+  margin: "0 auto",
+  gap: "2rem",
+};
 
 function App({ cards }) {
   const [cardData, setCardData] = useState(() => cards);
+  const [newCard, setNewCard] = useState({
+    company: "",
+    description: "",
+    notes: "",
+    service: "",
+  });
 
   return (
     <div className="App">
-      <Form />
+      <form style={styles}>
+        <input
+          type="text"
+          placeholder="Company"
+          name="company"
+          value={newCard.company}
+        />
+        <textarea
+          placeholder="Description"
+          name="description"
+          value={newCard.description}
+        />
+        <textarea placeholder="Notes" name="notes" value={newCard.notes} />
+        <label htmlFor="service">Service Level</label>
+        <select id="service" value={newCard.service} name="service">
+          <option value="walk-through">Walk Through</option>
+          <option value="super-express">Super-Express</option>
+          <option value="express">Express</option>
+          <option value="regular">Regular</option>
+          <option value="value">Value</option>
+        </select>
+      </form>
       {cardData.map((card) => (
         <Card card={card} />
       ))}
--- psa-db/src/components/Form.tsx
+++
@@ -1,9 +0,0 @@
-const Form = () => {
-  return (
-    <form>
-      <input type="text" placeholder="Company" name="company" />
-    </form>
-  );
-};
-
-export default Form;
