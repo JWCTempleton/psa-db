a1fcc7ca2c84c0b0d0937c91766c1ff95b396bde
Jacob Templeton <jacob.templeton@gmail.com>
2023-01-15 20:11:30 -0800

    Add data example to be served as JSON from the created server

--- card-backend/index.tsx
+++ card-backend/index.tsx
@@ -1,8 +1,61 @@
 const http = require("http");
 
+let cards = [
+  {
+    id: 1,
+    company: "Panini",
+    description: "2018-19 Panini Prizm #280 Luka Doncic (RC)",
+    notes: null,
+    service: "Express",
+    received: "2022-10-18",
+    submitted: "2022-10-20",
+    status: "pending",
+  },
+  {
+    id: 2,
+    company: "Upper Deck",
+    description: "2009-10 Upper Deck Star Rookies RC #234",
+    notes: null,
+    service: "Super Express",
+    received: "2022-10-21",
+    submitted: "2022-10-24",
+    status: "pending",
+  },
+  {
+    id: 3,
+    company: "Topps",
+    description: "2005-6 Chris Paul Topps Chrome Refractor Rookie Card #168",
+    notes: "Autographed",
+    service: "Super Express",
+    received: "2022-10-22",
+    submitted: "2022-10-25",
+    status: "pending",
+  },
+  {
+    id: 4,
+    company: "Fleer",
+    description: "Curry Card",
+    notes: "",
+    service: "super-express",
+    received: "2023-01-13T02:51:32.621Z",
+    submitted: null,
+    status: "pending",
+  },
+  {
+    id: 5,
+    company: "Topps",
+    description: "Durant Card",
+    notes: "Autographed",
+    service: "walk-through",
+    received: "2023-01-13T22:35:32.708Z",
+    submitted: null,
+    status: "pending",
+  },
+];
+
 const app = http.createServer((request, response) => {
-  response.writeHead(200, { "Content-Type": "text/plain" });
-  response.end("hello world!");
+  response.writeHead(200, { "Content-Type": "application/json" });
+  response.end(JSON.stringify(cards));
 });
 
 const PORT = 3001;
